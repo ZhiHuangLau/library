5bd382dfeed5c1613f1a19d3621c17a12d2d410b
ZhiHuangLau <lauzhihuang@gmail.com>
2025-01-06 16:19:11 +0800

    Add ability to add new books
    This commit adds the function for the user to add new books
    to the library by using a form and dialog. The current plan is
    for the table to be refreshed everytime a new book is added but a way
    to either clear the new branches or only append the final object in
    the array.

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const myLibrary=[Book={author:"me",title:"keke",pages:55,status:"yes"}];
+const myLibrary=[bruh={author:"me",title:"keke",pages:55,status:"yes"}];
+
+const dialog=document.querySelector("dialog");
+const showButton=document.querySelector("dialog+button");
+const closeButton=document.querySelector("#confirmBtn");
+const inputAut=dialog.querySelector("#author");
+const inputTit=dialog.querySelector("#title");
+const inputPg=dialog.querySelector("#pages");
+const inputRd=dialog.querySelector("#status");
+
+
 
 function Book(author,title,pages,status){
   this.author=author;
@@ -10,55 +20,80 @@ function Book(author,title,pages,status){
 
 
 function addBookToLibrary(author,title,pages,status){
-  
   const book1 =new Book(author,title,pages,status);
   myLibrary.push(book1);
 }
 
 
+showButton.addEventListener("click",() =>{
+  dialog.showModal();
+})
+
+
+
+
+
+
+
+
+
+
+
+
 let loopNum=1;
+function showTable(){
+  for(const book of myLibrary){
+    const container=document.querySelector(".container");
+    const row=document.createElement("div");
+    row.classList.add("table-row");
+    container.appendChild(row);
 
-for(const book of myLibrary){
-  const container=document.querySelector(".container");
-  const row=document.createElement("div");
-  row.classList.add("table-row");
-  container.appendChild(row);
+    const num=document.createElement("div");
+    num.classList.add("number");
+    num.textContent=loopNum;
+    row.appendChild(num);
+    loopNum++;
 
-  const num=document.createElement("div");
-  num.classList.add("number");
-  num.textContent=loopNum;
-  row.appendChild(num);
-  loopNum++;
+    const aut=document.createElement("div");
+    aut.classList.add("author");
+    aut.textContent=book.author;
+    row.appendChild(aut);
 
-  const aut=document.createElement("div");
-  aut.classList.add("author");
-  aut.textContent=book.author;
-  row.appendChild(aut);
+    const tit=document.createElement("div");
+    tit.classList.add("title");
+    tit.textContent=book.title;
+    row.appendChild(tit);
 
-  const tit=document.createElement("div");
-  tit.classList.add("title");
-  tit.textContent=book.title;
-  row.appendChild(tit);
+    const pg=document.createElement("div");
+    pg.classList.add("pages");
+    pg.textContent=book.pages;
+    row.appendChild(pg);
 
-  const pg=document.createElement("div");
-  pg.classList.add("pages");
-  pg.textContent=book.pages;
-  row.appendChild(pg);
+    const rd=document.createElement("div");
+    rd.classList.add("status");
+    rd.textContent=book.status;
+    row.appendChild(rd);
 
-  const rd=document.createElement("div");
-  rd.classList.add("status");
-  rd.textContent=book.status;
-  row.appendChild(rd);
+    const chgsts=document.createElement("button");
+    chgsts.classList.add("change-status-but")
+    chgsts.textContent="Change Read Status";
+    row.appendChild(chgsts);
 
-  const chgsts=document.createElement("button");
-  chgsts.classList.add("change-status-but")
-  chgsts.textContent="Change Read Status";
-  row.appendChild(chgsts);
+    const rmv=document.createElement("button");
+    rmv.classList.add("remove-book");
+    rmv.textContent="Remove Book";
+    row.appendChild(rmv);
+  
+  }}
 
-  const rmv=document.createElement("button");
-  rmv.classList.add("remove-book");
-  rmv.textContent="Remove Book";
-  row.appendChild(rmv);
- 
-}
+  showTable();
+
+
+  closeButton.addEventListener("click",(event) =>{
+    event.preventDefault();
+    dialog.close([inputAut.value,inputTit.value,inputPg.value,inputRd.value]); 
+    mek=dialog.returnValue.split(",");
+    addBookToLibrary(mek[0],mek[1],mek[2],mek[3]);
+    showTable();
+  })
 
